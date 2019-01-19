f9829b414df714d0cde421c4daada81b19c76229
Ade Adesina <janet.adesina@gmail.com>
2019-01-19 20:55:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function add2(n) {
   // Feel free to move things around!
 }
 
-var funkyFunction = function() {
-  return function() {
+function funkyFunction() {
+   return function() {
     return "FUNKY!"
   }
 }
